2f9f8f26699cd3cf47f3830375580769c9736e93
sabin999 <sabintendulkar999@gmail.com>
2020-07-01 21:08:56 +0300

    first commit

---
+++ app/controllers/PostController.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+
+<body ng-app="myApp">
+
+
+
+<a href="#!zzz">click here to route to post.html 1</a>
+
+
+<p>Click the Link above</p>
+
+<div ng-view></div>
+
+
+<script src="app/route.js"></script>
+</body>
+</html>
